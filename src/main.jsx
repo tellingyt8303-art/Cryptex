c3870c325d0d2641569b6fbc48ec68bd2def7ae3
tellingyt8303-art <tellingyt8303@gmail.com>
2026-04-08 07:55:26 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 
-export default defineConfig({
-  plugins: [react()],
-});
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
